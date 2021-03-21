baea6aa8e56e3b4d692f5a8b700b6db180d7034d
TomThomasPaul <70032157+TomThomasPaul@users.noreply.github.com>
2021-03-21 02:13:32 -0400

    Commiting changes for search recipe and recipe details

---
+++ MomsSpaghetti/Models/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MomsSpaghetti.Models
+{
+    public class Recipe
+    {
+        public String RecipeId { get; set; }
+
+        public String Title { get; set; }
+        public String Author { get; set; } 
+        public String Image { get; set; }
+        public String Url { get; set; }
+        public Array Ingredients { get; set; }
+    }
+}
