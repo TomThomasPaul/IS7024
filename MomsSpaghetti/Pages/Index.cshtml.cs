6d0f84b6eb356ad4a159b9c697aaec4229de146d
TomThomasPaul <70032157+TomThomasPaul@users.noreply.github.com>
2021-03-27 21:51:53 -0400

    commiting changes onPost and models

--- MomsSpaghetti/Pages/Index.cshtml.cs
+++ MomsSpaghetti/Pages/Index.cshtml.cs
@@ -27,6 +27,9 @@ namespace MomsSpaghetti.Pages
         public Recipe Recipe { get; set; }
 
         [BindProperty(SupportsGet = true)]
+        public Like Like { get; set; }
+
+        [BindProperty(SupportsGet = true)]
         public String recipeId { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger, IMemoryCache cache)
@@ -35,74 +38,18 @@ namespace MomsSpaghetti.Pages
             _cache = cache;
         }
 
-        public async Task<IActionResult> OnGet()
+        public void OnGet()
         { 
           
             
-            if(_cache.Get("searchRecipeResults") != null)
-            {
-
-                //ViewData["recipe"] = SearchRecipes.Result["recipes"][0]["title"];
-                //var temp = TempData["searchResultsString"];
-               var cachedResult= _cache.Get<JObject>("searchRecipeResults");
-                SearchRecipes.Result = cachedResult;
-
-            }
-
-            if (recipeId != null)
-            {
-                String recipeDetails = "";
-               // var recipeObject = new { };
-                try
-                { 
-                    HttpClient client = new HttpClient();
-
-                    // client.DefaultRequestHeaders.Add("q", UserInput);
-
-                    HttpResponseMessage response = await client.GetAsync("https://forkify-api.herokuapp.com/api/get?rId=" + recipeId);
-
-                    var temp = response;
-
-                    if (response.IsSuccessStatusCode)
-                    {
-                        recipeDetails = await response.Content.ReadAsStringAsync();
-                        JObject jsonObject = JObject.Parse(recipeDetails);
-                        Recipe.RecipeId = recipeId;
-                        Recipe.Title = jsonObject["recipe"]["title"].ToString();
-                        Recipe.Image = jsonObject["recipe"]["image_url"].ToString();
-                        Recipe.Author = jsonObject["recipe"]["publisher"].ToString();
-                        Recipe.Url = jsonObject["recipe"]["source_url"].ToString();
-                        Recipe.Ingredients = jsonObject["recipe"]["ingredients"].ToArray();
-
-                        
-                        //UserSearch SearchRecipes = new UserSearch();
-                        // recipeObject = jsonObject;
-                        // ViewData["result"] = SearchRecipes.Result["recipes"][0]["title"];
-
-                        /*  var props = SearchRecipes.Result.Properties();
-                         var  propsArray = props.ToArray();
-                          var first = propsArray[1].Value[0]["title"];  //Upto VALUE[0] gives the first recipe object
-
-
-                          */
-
-
-
-                    }
-
-                }
-                catch (Exception err)
-                {
-                    //return RedirectToPage("Error", err.Message);
-
-                }
-            }
-
-            return Page();
         }
 
-        public async Task<IActionResult>  OnPost()
+        public JsonResult OnPostAddLike( string msg)
         {
+            return new JsonResult(msg);
+        }
+        public async Task<IActionResult>  OnPost()
+        {   
             String responseContent="";
             var responseObject = new { };
             if (SearchRecipes.Query.Length != 0)
@@ -148,6 +95,8 @@ namespace MomsSpaghetti.Pages
                 }
 
             }
+
+       
 
             // return RedirectToPage("/ResultsPage", new { result = SearchRecipes.Result }) ;
             //return Content(responseContent);
