93b4779f3235b58684900dc6161f209d41df7739
TomThomasPaul <70032157+TomThomasPaul@users.noreply.github.com>
2021-03-27 22:07:11 -0400

    commiting latest changes

---
+++ MomsSpaghetti/Models/UserSearch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+namespace MomsSpaghetti.Models
+{
+    public class UserSearch
+    {
+        public String Query { get; set; }
+
+        public  JObject Result { get; set; }
+
+
+    }
+}
